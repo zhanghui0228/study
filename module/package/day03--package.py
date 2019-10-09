4259740895b9cb5c0f64b4654bfa3750df24faab
zh <helloworldhui@163.com>
2019-10-09 12:05:48 +0800

    更新python的包

--- module/package/day03--package.py
+++ module/package/day03--package.py
@@ -1,1 +1,18 @@
 #包
+'''
+    可以用来组织模块（可以包含其他模块的模块）
+    目录必须包含文件__init__.py
+    可以解决模块重名的问题
+
+    __init.py文件的作用：
+        将一个文件夹变为一个模块；
+        导入包实际就是导入它的__init.py；
+        一般为空，可以批量导入所需的模块
+
+'''
+
+import test_package
+print(dir(test_package))
+print(test_package.__package__)
+print(help(test_package))
+test_package
---
+++ module/package/test_package/tools.py
@@ -0,0 +1,6 @@
+'''
+    这是python包的测试练习
+'''
+
+def add():
+    print("test")
