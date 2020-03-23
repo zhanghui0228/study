8b851cdc9941941f51064181821eba6957cc7a04
hui <helloworldhui@163.com>
2020-03-23 16:42:06 +0800

    orm新增数据操作--  user.object.create()
    Signed-off-by: hui <helloworldhui@163.com>

---
+++ WebSite/Django/my_study/orm_create.py
@@ -0,0 +1,16 @@
+# ORM新增操作
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTING_MODULE', 'mydjango.settings')
+django.setup()
+from weibo.models import WeiboUser as user
+
+# 方法一:
+# user_A = user(username='user01', password='admin@123', nickname='admin')
+# user_A.save()
+# print(user_A.id)
+
+# 方法二：使用模型的create()进行新增数据
+user_B = user.objects.create(username='user02', password='admin@123', nickname='user02')
+print(user_B.pk)
